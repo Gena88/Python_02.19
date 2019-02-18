1f89a4a374d37dd70a918f86c2422d94e18bee87
Gena88 <32675403+Gena88@users.noreply.github.com>
2019-02-19 00:05:31 +0300

    refactor test

--- test_add_group.py
+++ test_add_group.py
@@ -7,11 +7,18 @@ class test_add_group(unittest.TestCase):
         self.wd = WebDriver()
         self.wd.implicitly_wait(8)
 
+
     def test_add_group(self):
-        success = True
         wd = self.wd
-        wd.get("http://localhost")
-        wd.find_element_by_link_text("groups").click()
+        self.open_home_page(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -23,8 +30,12 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("test")
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        self.assertTrue(success)
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost")
 
     def tearDown(self):
         self.wd.quit()
